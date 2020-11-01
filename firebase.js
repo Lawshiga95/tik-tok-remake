0fb0c37fe3d4bbb3538c7f26db4883ec70a0a8f2
Lawshiga95 <lawshiga123@gmail.com>
2020-11-01 18:36:12 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBH33NOGZFxMWF5dk4z_A3SFNDGj7kc06o",
+    authDomain: "tik-tok-remake-82018.firebaseapp.com",
+    databaseURL: "https://tik-tok-remake-82018.firebaseio.com",
+    projectId: "tik-tok-remake-82018",
+    storageBucket: "tik-tok-remake-82018.appspot.com",
+    messagingSenderId: "272456453698",
+    appId: "1:272456453698:web:8de90c2abd7d32c5c067db",
+    measurementId: "G-0025XFNCX4"
+  };
